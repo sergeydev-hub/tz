c56f7c4cf08b9ec7cd28acf2b1a1a4cdf2e1f6d7
sergeydev-hub <76453850+sergeydev-hub@users.noreply.github.com>
2021-03-21 01:37:15 +0400

    Update burger.js

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,15 @@
+let header = document.querySelector('.header')
+let burger = header.querySelector('.header__burger')
+let menu = header.querySelector('.header__menu')
+let btn = header.querySelector('.header__btn')
 
-let burger = document.querySelector('.header__burger')
-let menu = document.querySelector('.header__menu')
-let btn = document.querySelector('.header__btn')
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 50) {
+        header.classList.add('header_scroll')
+    } else {
+        header.classList.remove('header_scroll')
+    }
+})
 burger.addEventListener('click', event => {
     burger.classList.toggle('active')
     menu.classList.toggle('active')
